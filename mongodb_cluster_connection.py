09ee9f28ff2409826febb2306cd26166fb232631
Emre KARA <mailemrek@gmail.com>
2022-06-08 20:56:27 +0300

    Update mongodb_cluster_connection.py

--- mongodb_cluster_connection.py
+++ mongodb_cluster_connection.py
@@ -1,11 +1,11 @@
 from pymongo import MongoClient
 
-uri = "mongodb+srv://crypto-forecaster.bikyg.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
+uri = "mongodb+srv://crypto-forecaster.bikyg.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority" #DO NOT TOUCH HERE
 client = MongoClient(uri,
                      tls=True,
-                     tlsCertificateKeyFile='emre_X509-cert-7315245080984398576.pem')
+                     tlsCertificateKeyFile='<PEM_FILE_PATH>') #ADD YOUR PEM FILE PATH HERE AS STRING AND REMOVE <> SYMBOLS
 
-db = client['sample_airbnb']
-collection = db['listingsAndReviews']
-doc_count = collection.count_documents({})
-print(doc_count)
+db = client['sample_airbnb'] #SAMPLE DATABASE
+collection = db['listingsAndReviews'] #SAMPLE COLLECTION
+doc_count = collection.count_documents({}) #COUNTS DOCUMENTATION
+print(doc_count) #PRINTS DOCUMENTS COUNT
